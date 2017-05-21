4a25fedf0d0c601b263965fda6e67fcb7b138b27
fbleite <feliperbleite@gmail.com>
2017-05-21 15:04:59 -0400

    Finalized assignment with directives

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -3,7 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+  .bigNumber {
+    color: white;
+  }`]
 })
 export class AppComponent {
+  displaySecretPassword:boolean = false;
+  clicks = [];
+  logEntryNumber = 0;
+
+  onDisplayDetails() {
+    this.displaySecretPassword = !this.displaySecretPassword;
+    this.logEntryNumber ++;
+    this.clicks.push(new Date);
+  }
+
+  iDontCare() {
+    return true;
+  }
+  getColor(entryNumber) {
+      return entryNumber >= 4 ? 'blue': 'none';
+  }
 }
